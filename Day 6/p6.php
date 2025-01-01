55bfbe606699c55d67790d3d77aea0ce425c0d0a
AyaanIMansuri <154235858+AyaanIMansuri@users.noreply.github.com>
2025-01-01 09:47:26 -0800

    Add files via upload

---
+++ Day 6/p6.php
@@ -0,0 +1,12 @@
+<?php
+// Initialize the counter
+$number = 1;
+
+// Use a while loop to print odd numbers from 1 to 100
+while ($number <= 100) {
+    if ($number % 2 != 0) {
+        echo $number . " "."<br>";
+    }
+    $number++;
+}
+?>
