2aff60d302dbb13cec6a3bb488b3270c8494ae1f
AyaanIMansuri <154235858+AyaanIMansuri@users.noreply.github.com>
2025-01-04 20:42:30 -0800

    Add files via upload

---
+++ Day 9/Day9.php
@@ -0,0 +1,17 @@
+<?php
+// Function to check if a number is odd or even
+function checkOddOrEven($number) {
+    if ($number % 2 == 0) {
+        return "$number is Even";
+    } else {
+        return "$number is Odd";
+    }
+}
+
+// Example input
+$number = 20; // Change this value to test with other numbers
+
+// Check and display whether the number is odd or even
+$result = checkOddOrEven($number);
+echo $result;
+?>
